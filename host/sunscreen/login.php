65812bbbb7cc47d1a909f733ad4e6976d859cc12
Henry Meyer <henrymm@outlook.de>
2025-04-30 18:14:18 +0200

    General Update!

--- host/sunscreen/login.php
+++ host/sunscreen/login.php
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Witte-Meyer Login</title>
+    <title>SunScreen Website Look Login</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
