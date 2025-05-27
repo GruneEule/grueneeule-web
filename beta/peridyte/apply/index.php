6a899c2329a6d2e024c2fd64a456c4f694fbad7f
Henry Meyer <henrymm@outlook.de>
2025-05-27 18:07:03 +0200

    Beta Apply Discord OAuth2 Added!

--- beta/peridyte/apply/index.php
+++ beta/peridyte/apply/index.php
@@ -10,8 +10,9 @@ if (!isset($_SESSION['user'])) {
 
 $user = $_SESSION['user'];
 
-error_reporting(E_ALL);
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
 
 <!DOCTYPE html>
--- beta/peridyte/apply/oauth.php
+++ beta/peridyte/apply/oauth.php
@@ -54,6 +54,7 @@ curl_close($ch);
 header("Location: index.php");
 exit;
 
-error_reporting(E_ALL);
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
