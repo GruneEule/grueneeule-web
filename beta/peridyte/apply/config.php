820cfa7a1f4014a13f2ab81e6ec20a9d787e9490
Henry Meyer <henrymm@outlook.de>
2025-05-27 18:08:32 +0200

    Beta Apply Discord OAuth2 Added!

--- beta/peridyte/apply/config.php
+++ beta/peridyte/apply/config.php
@@ -2,7 +2,7 @@
 define('CLIENT_ID', '1357413552276766751');
 define('CLIENT_SECRET', '6WRfKhxPc3RdKawLOiLmfOPWx-hlgN6F');
 define('REDIRECT_URI', 'https://grueneeule.de/beta/peridyte/apply/oauth.php');
-define('BOT_TOKEN', 'DEIN_BOT_TOKEN'); // Falls du invite oder Guild-Join brauchst
+define('BOT_TOKEN', 'MTM1NzQxMzU1MjI3Njc2Njc1MQ.GMDr_L.o-_SxlFtkivak09mfhusPzeGf-H51xunT7v8fE'); // Falls du invite oder Guild-Join brauchst
 define('GUILD_ID', '1356197148810809484');
 define('WEBHOOK_URL', 'https://discord.com/api/webhooks/1376950043424067594/-U0mbe734t3q63ghK2KhHuXyLjiDo8vKRZd4Ih5qsI3xZ0EuKoiNLIjSkeXHIwJB80C5');
 ?>
