508d3a473adbc9707eb7c29036b0c7a559eaa321
JumpStone <143991347+Jumpstone@users.noreply.github.com>
2025-07-23 15:10:53 +0200

    V2.5.0

--- src/assets/js/load-header.js
+++ src/assets/js/load-header.js
@@ -5,3 +5,11 @@ window.addEventListener("DOMContentLoaded", () => {
             document.getElementById("header-container").innerHTML = data;
         });
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+    fetch("/assets/templates/header-wee.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-container-wee").innerHTML = data;
+        });
+});
